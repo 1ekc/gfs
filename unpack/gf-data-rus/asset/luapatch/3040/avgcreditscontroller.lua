f86744b37a3b1e29865089b4979d08e292d776e3
1ekc <1ekcui@gmail.com>
2025-05-19 02:44:06 +0300

    Delete unpack/gf-data-rus directory

--- unpack/gf-data-rus/asset/luapatch/3040/avgcreditscontroller.lua
+++
@@ -1,8 +0,0 @@
-local util = require 'xlua.util'
-xlua.private_accessible(CS.AVGCreditsController)
-local myStart = function(self)
-    self:Start();
-	CS.AVGController.Instance.goSkip:SetActive(false);
-end
-
-util.hotfix_ex(CS.AVGCreditsController,'Start',myStart)
--- unpack/gf-data-rus/asset/luapatch/3050/missionskilldata.lua
+++
@@ -1,8 +0,0 @@
-local util = require 'xlua.util'
-
-local deploymentTeam = function(self)
-	self._deploymentTeam = nil;
-	return self.deploymentTeam;
-end
-
-util.hotfix_ex(CS.BuffAction,'get_deploymentTeam',deploymentTeam)
